92aa1fa625fc4ed28bfdde7404c8c5bf8ed0ab9f
harshit-273 <kalavadiaharshit@gmail.com>
2023-01-23 12:17:21 +0530

    added loops

---
+++ Python/0011_Loop.py
@@ -0,0 +1,19 @@
+'''
+----------------------------------------------- Loop -----------------------------------------------
+
+- Loops are used to perform the repeated tasks.
+
+Syntax:
+
+	for val in sequence:
+		# statements
+	else:
+		# statements to be executed if the loops is terminated.
+
+	while condition:
+		# statements 
+
+'''
+
+for val in range(1, 6):
+	print(val,end = " ")
