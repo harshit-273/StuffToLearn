a8b9b1fa5b4998de0d2580ea82b6fdfc9fd07d6c
harshit-273 <kalavadiaharshit@gmail.com>
2023-01-23 12:31:29 +0530

    added pass

---
+++ Python/0013_Pass.py
@@ -0,0 +1,14 @@
+'''
+----------------------------------------------- pass -----------------------------------------------
+
+- "pass" statement is used when there is something to be implemented in future but for now wanted to be skipped.
+
+Syntax:
+
+	if condition:
+		pass
+
+'''
+
+if 5 > 4:
+	pass
