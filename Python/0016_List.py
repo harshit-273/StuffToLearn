b8753c04baf811a4cef96add3a3c51cc055922c5
harshit-273 <kalavadiaharshit@gmail.com>
2023-03-05 23:45:13 +0530

    added list and functions for it

--- Python/0016_List.py
+++ Python/0016_List.py
@@ -59,7 +59,7 @@ print(l[-2])
 '''
 
 '''
-Outut:
+Output:
 
 3rd element or last element
 2nd element or 2nd last element
@@ -76,9 +76,64 @@ Syntax:
 
 '''
 
-
+'''UNCOMMENTABLE BLOCK OF CODE
 l = [0, 1, 2, 3, 4]
 print(l[:])
 print(l[1:])
 print(l[:3])
 print(l[1:4])
+'''
+
+'''
+Output:
+
+[0, 1, 2, 3, 4]
+[1, 2, 3, 4]
+[0, 1, 2]
+[1, 2, 3]
+'''
+
+'''
+Python List Functions: --------------------------------------------------------------------------------------------------
+
+Syntax:
+
+	list_l1.append(value_to_be_appended)		// this will append the value in the parameter at the last
+    
+    del list_l1[start_index:index_after_end]	// this will delete the value based on the index, even a single element can be deleted
+    
+    list_l1.remove(value_to_be_removed)			// this will remove the value
+    
+    list_l1.extend(iterator)					// this will add the interator at the end of the list
+    
+    list_l1.insert(index, value)				// this will add the value at the specified index, values from the index will move next to the index
+    
+    list_l1.pop(index)							// this will remove and return the value of the list at the index, default value of index here is -1
+    
+    list_l1.index(value)						// this will return the index of the specified value
+    
+    list_l1.reverse()							// this will reverse the list
+    
+    list_l1.count(value_to_be_counted)			// this will return the number of count of the value passed
+'''
+
+list_l1 = ["one", "two", "three", "four", "five", "six", "seven"]
+
+list_l1.append("eight")
+
+del list_l1[7]
+
+list_l1.remove("seven")
+
+list_l1.extend(["seven", "eight", "nine"])
+
+ret_index = list_l1.index(5)
+
+list_l1.insert(0, "zero")
+
+list_l1.pop()
+
+list_l1.reverse()
+
+list_l1.count("two")
+
