1ed7159bb8a14eb07074b9ec3c00c0377facfc93
harshit-273 <kalavadiaharshit@gmail.com>
2024-01-03 19:53:11 +0530

    LearALanguage - golang

--- GO/Codes/003_PrintfAndSprintf.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var someVariable string = fmt.Sprintf("%cNameste%c my name is %s and I am %d years old", '🙏', '🙏', "Harshit", 22)
-
-	fmt.Printf("%v", someVariable)
-}
