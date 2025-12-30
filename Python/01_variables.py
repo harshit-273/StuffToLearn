ebef73b4770cb3d77f52c8d19fb5ab1da85b7b14
harshit-273 <kalavadiaharshit@gmail.com>
2025-12-31 00:15:18 +0530

    python variables

---
+++ Python/01_variables.py
@@ -0,0 +1,43 @@
+# strings
+some_variable = "some value"
+
+print(some_variable)
+
+print(f"Printing the value \"{some_variable}\" for variable \"some_variable\"")
+
+"""
+Output:
+some value
+Printing the value "some value" for variable "some_variable"
+"""
+
+# integers
+some_integer = 5
+
+print(some_integer)
+
+"""
+Output:
+5
+"""
+
+# float
+some_float = 10.00
+
+print(f"My package is {some_float} LPA")
+
+"""
+Output:
+My package is 10.0 LPA
+"""
+
+# boolean
+is_true = True
+is_false = False
+
+print(f"Is the variable \"is_true\" true? \"{is_true}\"")
+
+"""
+Output:
+Is the variable "is_true" true? "True"
+"""
