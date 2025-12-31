af0c1ec8cd0537580974325009b59d9f5338d4fb
harshit-273 <kalavadiaharshit@gmail.com>
2025-12-31 06:06:33 +0530

    operators in python

---
+++ Python/04_operators.py
@@ -0,0 +1,76 @@
+# Arithmetic Operators
+print(1+2)
+print("ab"+"yz")
+print(5-2)
+print(3*4)
+print(5/2)
+print(5//2)
+print(5**2)
+print(5%2)
+
+"""
+Output:
+3
+abyz
+3
+12
+2.5
+2
+25
+1
+"""
+
+# Comparison Operators
+
+print(5<2)
+print(5>2)
+print(5<=5)
+print(5>=5)
+print(2==4)
+print(2!=4)
+print(["apple", "banana", "cherry"] is ["apple", "banana", "cherry"])
+print(2 in [1, 2, 3])
+
+"""
+Output:
+False
+True
+True
+True
+False
+True
+False
+True
+"""
+
+# Bitwise Operators
+
+print(1&0)
+print(1|0)
+print(~0)
+print(3^1)
+print(1<<2)
+print(4>>2)
+
+"""
+Output:
+0
+1
+-1
+2
+4
+1
+"""
+
+# Logical Operators
+
+print(1<2 and 3>2)
+print(1>2 or 3<2)
+print(not True)
+
+"""
+Output:
+True
+False
+False
+"""
