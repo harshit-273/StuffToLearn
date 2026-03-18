a48b642f29b0fa27823f14845d438849ea4ca9fd
harshit-273 <kalavadiaharshit@gmail.com>
2026-03-18 16:15:18 +0530

    maths operations in python

---
+++ Python/05_maths_operation.py
@@ -0,0 +1,21 @@
+"""
+round(), abs(), max(), min(), math.ceil()
+"""
+import math
+
+print(round(3.14, 1))
+print(abs(-4))
+print(max(4, 3, 7))
+print(min(2, 0, -1))
+print(math.ceil(4.01), math.ceil(-4.01))
+print(math.floor(4.9), math.floor(-4.9))
+
+"""
+Output:
+3.1
+4
+7
+-1
+5 -4
+4 -5
+"""
