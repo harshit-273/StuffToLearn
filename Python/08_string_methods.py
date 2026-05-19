0867933cba098045b1642f7440088827d3c0cd5f
harshit-273 <kalavadiaharshit@gmail.com>
2026-05-19 17:11:47 +0530

    string methods

---
+++ Python/08_string_methods.py
@@ -0,0 +1,28 @@
+"""
+len(), find(), rfind(), capitalize(), upper(), lower(), isdigit(), isalpha(), count(), replace() 
+"""
+st = "harsh it"
+print(len(st))
+print(st.find("h"))
+print(st.rfind("h"))
+print(st.capitalize())
+print(st.upper())
+print(st.lower())
+print(st.isdigit())
+print(st.isalpha())
+print(st.count("h"))
+print(st.replace(" ", ""))
+
+"""
+Output:
+8
+4
+4
+Harsh it
+HARSH IT
+harsh it
+False
+False
+2
+harshit
+"""
