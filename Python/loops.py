6701c6b94c3eee52fca4ede749ca9f3db39b0678
harshit-273 <kalavadiaharshit@gmail.com>
2026-05-22 19:24:41 +0530

    break and continue

--- Python/loops.py
+++ Python/loops.py
@@ -26,3 +26,26 @@ Output:
 1 2 3 4 5 6 7 8 9 10
 """
 
+# break
+
+while True:
+	if "y" == input("Do you want to exit this loop?(Press y to exit):"):
+		break
+
+print("You have exitted the loop")
+
+# continue
+# printing multiples of 3 or 5 but not of 3 and 5.
+
+for i in range(0, 50, 1):
+	if (i%3 == 0) and (i%5 == 0):
+		continue
+	elif i%3 == 0:
+		print(i, end=" ")
+	elif i%5 == 0:
+		print(i, end=" ")
+
+"""
+Output:
+3 5 6 9 10 12 18 20 21 24 25 27 33 35 36 39 40 42 48
+"""
