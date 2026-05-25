26148361efb1fcf2bcb1051dee01cbb8cbb0b9d7
harshit-273 <kalavadiaharshit@gmail.com>
2026-05-25 17:45:02 +0530

    tuple

---
+++ Python/13_tuple.py
@@ -0,0 +1,13 @@
+# Tuple - ()
+
+tup = (1, True, 0, False)
+print(tup)
+print(tup.index(True)) # gives 0 as, True is treated same a integer is treated
+print(tup.count(0)) # 0 and False both are counted as same so gives 2.
+
+"""
+Output:
+(1, True, 0, False)
+0
+2
+"""
